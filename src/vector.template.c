6b1256e9103a9f8353bcf0ad81e958eda7ae50a1
coreyp_1 <pennycuff.c@gmail.com>
2024-02-03 00:41:54 -0600

    fix vector off-by-one error on capacity comparison

--- src/vector.template.c
+++ src/vector.template.c
@@ -81,7 +81,7 @@ bool TEMPLATE_GCU_VECTOR_RESERVE(TEMPLATE_GCU_VECTOR * vector, size_t size) {
   }
 
   // Verify that the requested size is larger than the current capacity.
-  if (size <= vector->capacity) {
+  if (size < vector->capacity) {
     return false;
   }
 
